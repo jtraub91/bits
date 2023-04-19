d8ce2fb8a37fc128075b476270aa089d1ff17120
Jason Traub <jtraub91@gmail.com>
2023-04-19 01:14:49 -0400

    remove cli methods and tests not staged for v0.1.0

--- tests/integration/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from bits.integrations import generate_funded_keys
-
-
-@pytest.fixture(scope="module")
-def funded_keys_101():
-    return generate_funded_keys(101, network="regtest")
